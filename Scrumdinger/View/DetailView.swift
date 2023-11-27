8f34df17d6adea9048f74b9803b389a351f8d60b
Batuhan Demirbaş <batuhan.demirbas@bgnetmobile.com>
2023-11-27 16:20:19 +0300

    Creating the edit view

--- Scrumdinger/View/DetailView.swift
+++ Scrumdinger/View/DetailView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct DetailView: View {
     let scrum: DailyScrum
     
+    @State private var isPresentingEditView = false
+    
     var body: some View {
         List {
             Section("Meeting Info") {
@@ -31,6 +33,7 @@ struct DetailView: View {
                         .padding(4)
                         .background(scrum.theme.mainColor)
                         .cornerRadius(4)
+                    
                 }
             }
             
@@ -42,6 +45,30 @@ struct DetailView: View {
             
         }
         .navigationTitle(scrum.title)
+        .sheet(isPresented: $isPresentingEditView) {
+            NavigationStack {
+                DetailEditView()
+                    .navigationTitle(scrum.title)
+                    .toolbar {
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button("Cancel") {
+                                isPresentingEditView = false
+                            }
+                        }
+                        
+                        ToolbarItem(placement: .confirmationAction) {
+                            Button("Done") {
+                                isPresentingEditView = false
+                            }
+                        }
+                    }
+                
+            }
+        }.toolbar {
+            Button("Edit") {
+                isPresentingEditView = true
+            }
+        }
     }
 }
 
